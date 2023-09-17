1d2c2fdd2c7b9fdd3214fd026b884bd88eb78733
Armand <72499921+brandnewchoppa@users.noreply.github.com>
2023-09-17 13:24:04 +0200

    Update rope_tensorflow.py

--- rope_tensorflow/rope_tensorflow.py
+++ rope_tensorflow/rope_tensorflow.py
@@ -4,7 +4,7 @@ from keras.layers import Layer
 
 def rotate_half(x):
     x_shape = x.shape
-    x = tf.reshape(x, x.shape[:-1] + tf.TensorShape([ x.shape[-1] // 2, 2 ]))
+    x = tf.reshape(x, tf.TensorShape(x.shape[:-1] + [ x.shape[-1] // 2, 2 ]))
     x1, x2 = tf.split(x, 2, axis = -1)
     #x1, x2 = tf.squeeze([x1, x2], axis = -1)
     x1, x2 = tf.squeeze(x1, axis = -1), tf.squeeze(x2, axis = -1)
